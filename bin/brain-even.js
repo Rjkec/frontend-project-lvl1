995253e813867eed518f82b4479c9bc0cb9ff031
Nick Tikhomirov <klaus_silen@mail.ru>
2021-12-16 16:10:18 +0300

    delete old brain-even.js rename brain-logic to brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,27 +1,70 @@
-#!/usr/bin/env node
-
+import readlineSync from 'readline-sync';
 import {hi} from '../src/cli.js';
-import {brainEven} from './brain-logic.js';
 
 const name = hi();
+
+export const brainEven = () => {
+    
+    function getRndInteger(min, max) {
+         return Math.floor(Math.random() * (max - min) ) + min;
+    }
+    let number = getRndInteger(0,100);
+    
+    const question = ('Question: ' + `${number}`);
+    
+    console.log(question);
+    
+    const answer = readlineSync.question('Your answer: ');
+        
+        if (number % 2 === 0) {
+    
+            if (answer === 'yes'){
+    
+                console.log('Correct!');
+                return true;
+            }
+            if (answer === 'no'){
+    
+                console.log('no is wrong answer ;(. Correct answer was yes');
+                console.log ('Let\'s try again, ' + name + '!');
+                return false;
+                
+            } 
+        }
+        if (number % 2 !== 0) {
+    
+            if (answer === 'yes'){
+    
+                console.log('yes is wrong answer ;(. Correct answer was no');
+                console.log ('Let\'s try again, ' + name + '!');
+                return false;
+            }
+            if (answer === 'no'){
+    
+                console.log('Correct!');
+                return true;
+    
+            } 
+        }
+    
+}
 
 let i = 0;
 while (i < 3) {
 
     const result = brainEven();
     //brainEven()
-    console.log('counter',i);
+    //console.log('counter',i);
     if (result === true) {
         i++;
-        console.log('counter++',i);
+        //console.log('counter++',i);
         //continue;
     }
     
     if (result === false) {
-        console.log('yes, its happening');
+        //console.log('yes, its happening');
         i = 0;
-        console.log('final i',i);
+        //console.log('final i',i);
     }
 }
 console.log('Congratulations, ' + name + '!');
-
