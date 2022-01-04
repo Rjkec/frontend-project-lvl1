a5874ef10abbcc77c7bdd800e2acea2ad5f91811
Nick Tikhomirov <klaus_silen@mail.ru>
2022-01-04 19:02:03 +0300

    fix indent problem

--- bin/games/brain-progression.js
+++ bin/games/brain-progression.js
@@ -1,63 +1,63 @@
 #!/usr/bin/env node
 import { hi } from '../../src/cli.js';
 import {
-    randomNumber, userAnswer,
+  randomNumber, userAnswer,
 } from '../../src/index.js';
 
 const name = hi();
 console.log('What number is missing in the progression?');
 
 const brainProgression = () => {
-    const stepOfProgression = randomNumber();
-    // console.log('Step',stepOfProgression);
-    const startOfProgression = randomNumber();
-    const progression = [startOfProgression];
+  const stepOfProgression = randomNumber();
+  // console.log('Step',stepOfProgression);
+  const startOfProgression = randomNumber();
+  const progression = [startOfProgression];
 
-    for (let i = 1; progression.length < 10; i++) {
-        progression[i] = stepOfProgression + progression[i - 1];
-        // console.log(progression[i]);
-    }
+  for (let i = 1; progression.length < 10; i++) {
+    progression[i] = stepOfProgression + progression[i - 1];
+    // console.log(progression[i]);
+  }
 
-    function getRndInteger(min, max) {
-        return Math.floor(Math.random() * (max - min)) + min;
-    }
-    const randomNum = getRndInteger(0, 10);
+  function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+  const randomNum = getRndInteger(0, 10);
 
-    const saveProgression = progression[randomNum];
-    progression[randomNum] = '..';
+  const saveProgression = progression[randomNum];
+  progression[randomNum] = '..';
 
-    console.log(`Question: ${progression.join(' ')}`);
-    const answer = +userAnswer();
+  console.log(`Question: ${progression.join(' ')}`);
+  const answer = +userAnswer();
 
-    if (answer === saveProgression) {
-        console.log('Correct!');
-        return true;
-    }
+  if (answer === saveProgression) {
+    console.log('Correct!');
+    return true;
+  }
 
-    if (answer !== saveProgression) {
-        console.log(`${answer} is wrong answer ;(. Correct answer was ${saveProgression}`);
-        console.log(`Let's try again, ${name}!`);
-        return false;
-    }
-    return null; // ADD THIS JUST TO FIX LINTER
+  if (answer !== saveProgression) {
+    console.log(`${answer} is wrong answer ;(. Correct answer was ${saveProgression}`);
+    console.log(`Let's try again, ${name}!`);
+    return false;
+  }
+  return null; // ADD THIS JUST TO FIX LINTER
 };
 let i = 0;
 while (i < 3) {
-    const result = brainProgression();
-    // console.log('counter',i);
-    if (result === true) {
-        /* eslint-disable no-plusplus */
-        i++;
-        // console.log('counter++',i);
-        // continue;
-        if (i === 3) {
-            console.log(`Congratulations, ${name}!`);
-        }
+  const result = brainProgression();
+  // console.log('counter',i);
+  if (result === true) {
+    /* eslint-disable no-plusplus */
+    i++;
+    // console.log('counter++',i);
+    // continue;
+    if (i === 3) {
+      console.log(`Congratulations, ${name}!`);
     }
+  }
 
-    if (result === false) {
-        // console.log('yes, its happening');
-        break;
-        // console.log('final i',i);
-    }
+  if (result === false) {
+    // console.log('yes, its happening');
+    break;
+    // console.log('final i',i);
+  }
 }
