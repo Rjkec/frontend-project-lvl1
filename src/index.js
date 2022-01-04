f7eee1f80ecc8d98c585500ab203fa8755fdcd3e
Nick Tikhomirov <klaus_silen@mail.ru>
2022-01-04 19:13:05 +0300

    fix index and cli js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
-}   
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
-export const randomNumber = () => { 
-    const number = getRndInteger(1,10);
-    return number;
+export const randomNumber = () => {
+  const number = getRndInteger(1, 10);
+  return number;
 };
-export const userQuestion = () => { 
-    const number = randomNumber();
-    const question ="Question: " + `${number}`;
-    console.log(question);
-    return number;  
+export const userQuestion = () => {
+  const number = randomNumber();
+  const question = `Question: ${number}`;
+  console.log(question);
+  return number;
 };
 export const userAnswer = () => {
-    const answer = readlineSync.question("Your answer: ");
-    return answer;
+  const answer = readlineSync.question('Your answer: ');
+  return answer;
 };
