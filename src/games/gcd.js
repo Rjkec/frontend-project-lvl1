0a538a99aa6c5154a5ffc0f7509743156cc8c248
Nick Tikhomirov <klaus_silen@mail.ru>
2022-01-05 19:20:54 +0300

    fix all games

---
+++ src/games/gcd.js
@@ -0,0 +1,67 @@
+import {
+  cons, car, cdr,
+} from '@hexlet/pairs';
+import greeting from '../cli.js';
+import {
+  randomNumber, userAnswer,
+} from '../index.js';
+
+const gcdGame = () => {
+  const name = greeting();
+  console.log('Find the greatest common divisor of given numbers.');
+
+  const brainGcd = () => {
+    const pair = cons(randomNumber(), randomNumber());
+    console.log(`Question: ${car(pair)} ${cdr(pair)}`);
+    const answer = +userAnswer();
+
+    const egcd = (a, b) => {
+      if (a === 0) return b;
+
+      while (b !== 0) {
+        if (a > b) {
+          /* eslint-disable no-param-reassign */
+          a -= b;
+        } else b -= a;
+      }
+
+      return a;
+    };
+
+    if (egcd(car(pair), cdr(pair)) === answer) {
+      console.log('Correct!');
+      return true;
+    }
+    if (egcd(car(pair), cdr(pair)) !== answer) {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${egcd(car(pair), cdr(pair))}`);
+      console.log(`Let's try again, ${name}!`);
+      return false;
+    }
+    return null; // ADD THIS JUST TO FIX LINTER
+  };
+
+  let i = 0;
+  while (i < 3) {
+    const result = brainGcd();
+    // console.log(result);
+    // console.log('counter',i);
+
+    if (result === true) {
+      /* eslint-disable no-plusplus */
+      i++;
+      // console.log('counter++',i);
+      // continue;
+      if (i === 3) {
+        console.log(`Congratulations, ${name}!`);
+      }
+    }
+
+    if (result === false) {
+      // console.log('AAAyes, its happening');
+      break;
+      // console.log('final i',i);
+    }
+  }
+};
+
+export default gcdGame;
---
+++ src/games/prime.js
@@ -0,0 +1,73 @@
+import greeting from '../cli.js';
+import {
+  randomNumber, userAnswer,
+} from '../index.js';
+
+const primeGame = () => {
+  const name = greeting();
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+  const brainPrime = () => {
+    const number = randomNumber();
+    console.log(`Question: ${number}`);
+    const answer = userAnswer();
+
+    const isPrime = (num) => {
+      if (num === 1) {
+        return false;
+      }
+      /* eslint-disable no-plusplus */
+      for (let i = 2; i < num; i++) if (num % i === 0) return false;
+      return true;
+    };
+
+    if (isPrime(number) === true) {
+      if (answer === 'yes') {
+        console.log('Correct!');
+        return true;
+      }
+      if (answer === 'no') {
+        console.log('no is wrong answer ;(. Correct answer was yes');
+        console.log(`Let's try again, ${name}!`);
+        return false;
+      }
+    }
+
+    if (isPrime(number) === false) {
+      if (answer === 'yes') {
+        console.log('yes is wrong answer ;(. Correct answer was no');
+        console.log(`Let's try again, ${name}!`);
+        return false;
+      }
+      if (answer === 'no') {
+        console.log('Correct!');
+        return true;
+      }
+    }
+    return null; // ADD THIS JUST TO FIX LINTER
+  };
+
+  let i = 0;
+  while (i < 3) {
+    const result = brainPrime();
+    // console.log(result);
+    // console.log('counter',i);
+
+    if (result === true) {
+    /* eslint-disable no-plusplus */
+      i++;
+      // console.log('counter++',i);
+      // continue;
+      if (i === 3) {
+        console.log(`Congratulations, ${name}!`);
+      }
+    }
+
+    if (result === false) {
+    // console.log('AAAyes, its happening');
+      break;
+    // console.log('final i',i);
+    }
+  }
+};
+export default primeGame;
