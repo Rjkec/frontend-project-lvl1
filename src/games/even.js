214f89538812339bd8cdae1a0e14eacc20a44430
Nick Tikhomirov <klaus_silen@mail.ru>
2022-01-05 19:08:02 +0300

    change structure of brain-calc and brain-even

---
+++ src/games/even.js
@@ -0,0 +1,58 @@
+import greeting from '../cli.js';
+import { userQuestion, userAnswer } from '../index.js';
+
+const evenGame = () => {
+  const name = greeting();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const brainEven = () => {
+    const number = userQuestion();
+    const answer = userAnswer();
+
+    if (number % 2 === 0) {
+      if (answer === 'yes') {
+        console.log('Correct!');
+        return true;
+      }
+      if (answer === 'no') {
+        console.log('no is wrong answer ;(. Correct answer was yes');
+        console.log(`Let's try again, ${name}!`);
+        return false;
+      }
+    }
+    if (number % 2 !== 0) {
+      if (answer === 'yes') {
+        console.log('yes is wrong answer ;(. Correct answer was no');
+        console.log(`Let's try again, ${name}!`);
+        return false;
+      }
+      if (answer === 'no') {
+        console.log('Correct!');
+        return true;
+      }
+    }
+    console.log(`Let's try again, ${name}!`);
+    return false;
+  };
+
+  let i = 0;
+  while (i < 3) {
+    const result = brainEven();
+    // console.log('counter',i);
+    if (result === true) {
+    /* eslint-disable no-plusplus */
+      i++;
+      // console.log('counter++',i);
+      // continue;
+      if (i === 3) {
+        console.log(`Congratulations, ${name}!`);
+      }
+    }
+
+    if (result === false) {
+    // console.log('yes, its happening');
+      break;
+    // console.log('final i',i);
+    }
+  }
+};
+export default evenGame;
